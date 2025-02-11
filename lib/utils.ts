91867d29dc7b2040edc5105da9887f7ac608d143
unknown <2020583117@qq.com>
2025-02-11 22:21:11 +0800

    feat: 代码格式化相关配置

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,29 +1,23 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
-const BASE_URL = "https://bilibili.github.io/WebAV";
-export function assetsPrefix<T extends string[] | Record<string, string>>(
-	assetsURL: T
-): T {
-	const prefix = process.env.NODE_ENV === "development" ? "/" : "/WebAV/";
-	if (Array.isArray(assetsURL)) {
-		return assetsURL.map((url) => `${BASE_URL}${prefix}${url}`) as T;
-	}
+const BASE_URL = "https://bilibili.github.io/WebAV"
+export function assetsPrefix<T extends string[] | Record<string, string>>(assetsURL: T): T {
+  const prefix = process.env.NODE_ENV === "development" ? "/" : "/WebAV/"
+  if (Array.isArray(assetsURL)) {
+    return assetsURL.map((url) => `${BASE_URL}${prefix}${url}`) as T
+  }
 
-	return Object.fromEntries(
-		Object.entries(assetsURL).map(([k, v]) => [k, `${BASE_URL}${prefix}${v}`])
-	) as T;
+  return Object.fromEntries(Object.entries(assetsURL).map(([k, v]) => [k, `${BASE_URL}${prefix}${v}`])) as T
 }
 
-export async function createFileWriter(
-	extName = "mp4"
-): Promise<FileSystemWritableFileStream> {
-	const fileHandle = await window.showSaveFilePicker({
-		suggestedName: `WebAV-export-${Date.now()}.${extName}`,
-	});
-	return fileHandle.createWritable();
+export async function createFileWriter(extName = "mp4"): Promise<FileSystemWritableFileStream> {
+  const fileHandle = await window.showSaveFilePicker({
+    suggestedName: `WebAV-export-${Date.now()}.${extName}`
+  })
+  return fileHandle.createWritable()
 }
